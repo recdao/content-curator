09742bea3c86be4190a013dae16e05595dc1f556
Carl Larson <cslarson@gmail.com>
2018-04-04 16:20:34 +0100

    log contract address on load

--- bot/initWeb3.js
+++ bot/initWeb3.js
@@ -4,12 +4,14 @@ const writeFileAsync = Promise.promisify(require("fs").writeFile);
 const providerUrl = require('../config').providerUrl;
 const Web3 = require("web3");
 const ContentDAOArtifacts = require("../contracts/build/contracts/ContentDAO.json");
+const address = ContentDAOArtifacts.networks["4"].address;
+console.log("ContentDAO address:", address);
 
 exports.contracts = {ContentDAO: null};
 exports.reset = function(){
   // new Web3("http://127.0.0.1:9545/");
   let web3 = new Web3(providerUrl);
-  this.contracts.ContentDAO = new web3.eth.Contract(ContentDAOArtifacts.abi, ContentDAOArtifacts.networks["4"].address);
+  this.contracts.ContentDAO = new web3.eth.Contract(ContentDAOArtifacts.abi, address);
   subscribeFlips(this.contracts.ContentDAO);
   subscribeOpens(this.contracts.ContentDAO);
   subscribeBlocks(web3);
